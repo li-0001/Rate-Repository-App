5ebb37c922d057b98d92800d7d7d1fa823609979
Audrina <lf.audrina@gmail.com>
2022-01-08 15:48:32 -0800

    Added platform specific fonts

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,5 @@
+import { Platform } from 'react-native';
+
 const theme = {
     colors: {
         primary: '#0366d6',
@@ -12,7 +14,11 @@ const theme = {
         subheading: 16,
     },
     fonts: {
-        main: 'System',
+        main: Platform.select({
+            default: 'System',
+            android: 'Roboto',
+            ios: 'Arial',
+        }),
     },
     fontWeights: {
         normal: '400',
